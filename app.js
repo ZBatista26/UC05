e307bb2a6e371f59e2db0d62877ddb62bd4e39a0
Batista <franciscobatista744@gmail.com>
2025-02-06 17:44:15 -0300

    add: compremento atividade do dia 06/02/2025

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, resposta) => {
-  resposta.send('Hello World!')
+//Importando o express
+const express = require('express');
+
+// Inmportar a biblioteca dotenv
+const dotenv = require('dotenv');
+
+//criando as variaveis de aambiente
+dotenv.config();
+
+// criando um objeto chamado app
+const aplicativo = express()
+
+//Criando uma porta
+const port = process.env.PORTA
+
+// Meio termo - middlewarse
+aplicativo.use(express.json())
+
+// pegar - get
+// postar - post
+// atualizar - put
+// Deletar - Delete 
+
+const banco_dados = [];
+
+// Rota de (listar) os proddutos 
+aplicativo.get('/produtos', (req, res) => {
+  res.json(banco_dados)
 })
 
-app.listen(port, () => {
-  console.log(`Seervidor roddando na porta 3000 ${port}`)
+// Rota de (postagem) os produtos
+aplicativo.post('/produtos', (req, res) => {
+    const{id, nome, preco} = req.body; // passando na reqquisição o id, nome e preço
+
+    const novoProduto = {id, nome, preco}; //  adicionando a variavel novoProduto os dados que enviei no body(corpo)
+
+    banco_dados.push(novoProduto) // adicionando ao array banco_dados
+
+    res.status(201).json({mensage: "Produto criado com sucesso"}) // adicionando uma resposta de sucesso se o recurso ou o produto for criado 
+  })
+
+aplicativo.listen(port, () => {
+  console.log(`Seervidor rodando em http://localhost:${port}`)
 })
